709471d05933056cd5492e967346206b6fc98d80
iancarpenter <ianhcarpenter@gmail.com>
2020-07-28 22:09:09 +0100

    mvp finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,12 +15,13 @@ import { JokeCategoriesComponent } from './joke-categories/joke-categories.compo
     AppComponent,
     HomeComponent,
     RandomJokeComponent,
-    JokeCategoriesComponent,        
+    JokeCategoriesComponent,            
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/joke-categories/joke-categories.component.ts
+++ src/app/joke-categories/joke-categories.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JokeService } from '../shared/joke.service';
+import { Joke } from '../shared/joke';
 
 @Component({
   selector: 'app-joke-categories',
@@ -9,12 +10,22 @@ import { JokeService } from '../shared/joke.service';
 export class JokeCategoriesComponent implements OnInit {
   
   categories: string[];
+  selectedCategory: string;
+  categoryJoke: Joke;
 
   constructor(private jokeService: JokeService) { }
 
   ngOnInit(): void {
     this.jokeService.getJokeCategories().subscribe(data => {
       this.categories = data;
+    });
+    // need to add these to a forms drop down component
+  }
+
+  getJokeCategory(): void {
+    console.log('The cat selected is ' + this.selectedCategory);
+    this.jokeService.getRandomJokeFromCategory(this.selectedCategory).subscribe( data => {
+      this.categoryJoke = data;
     });
   }
 
